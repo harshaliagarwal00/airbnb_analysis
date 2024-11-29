c9f54734afd061f9b31edd09eb257d87d2fc3ef4
harshaliagarwal00 <harshaliagarwal00@gmail.com>
2024-11-29 12:26:42 +0530

    Update Airbnb.py
    err3

--- Airbnb.py
+++ Airbnb.py
@@ -1,25 +1,5 @@
 import streamlit as st
 
-# Setting up page configuration
-st.set_page_config(page_title= "Airbnb Data Visualization | By Jafar Hussain",
-                   page_icon= icon,
-                   layout= "wide",
-                   initial_sidebar_state= "expanded",
-                   menu_items={'About': """# This dashboard app is created by *Jafar Hussain*!
-                                        Data has been gathered from mongodb atlas"""}
-                  )
-
-# Creating option menu in the side bar
-with st.sidebar:
-    selected = option_menu("Menu", ["Home","Overview","Explore"], 
-                           icons=["house","graph-up-arrow","bar-chart-line"],
-                           menu_icon= "menu-button-wide",
-                           default_index=0,
-                           styles={"nav-link": {"font-size": "20px", "text-align": "left", "margin": "-2px", "--hover-color": "#FF5A5F"},
-                                   "nav-link-selected": {"background-color": "#FF5A5F"}}
-                          )
-
-
 # HOME PAGE
 if selected == "Home":
     # Title Image
